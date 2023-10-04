bf9e6a18d135c6ca7f7183ace4ab5ff1e7c84881
rysiek <kuba.urbanski@onet.eu>
2023-10-04 13:18:50 +0200

    sesje

---
+++ sesje/session2.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+echo 'welcome to page #2<br>';
+echo 'ulubiony kolor: '.$_SESSION['favcolor'];
+echo 'ulubione zwierze : '.$_SESSION['animal'].'<br>';
+echo date ( 'Y m d H:i:s', $_SESSION['time']). '<br>';
+echo $_SESSION['bzdura'];
+?>
